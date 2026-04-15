c20bd50c1774600630b4c21feea4740d5fd82fd0
Teja <tejaambati20@gmail.com>
2026-04-15 19:47:36 +0530

    Implement logging functionality with Log4j and update main.py for logging session creation and completion

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import sys
-from lib import DataManupulation, DataReader, Utils
+from lib import DataManupulation, DataReader, Utils, logger
 from pyspark.sql.functions import *
+from lib.logger import Log4j
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Please Specify the environment")
+        print("Please specify the environment")
         sys.exit(-1)
+
     job_run_env = sys.argv[1]
+
     print("Creating Spark Session")
+
     spark = Utils.get_spark_session(job_run_env)
-    print("Created Spark Session")
-    orders_df = DataReader.read_orders(spark, job_run_env)
+
+    logger = Log4j(spark)
+
+    logger.warn("Created Spark Session")
+
+    orders_df = DataReader.read_orders(spark,job_run_env)
+
     orders_filtered = DataManupulation.filter_closed_orders(orders_df)
-    customers_df = DataReader.read_customers(spark, job_run_env)
-    joined_df = DataManupulation.join_orders_customers(orders_filtered, customers_df)
-    aggregateed_results = DataManupulation.count_orders_state(joined_df)
-    aggregateed_results.show(100)
-    print("end of main")
+
+    customers_df = DataReader.read_customers(spark,job_run_env)
+
+    joined_df = DataManupulation.join_orders_customers(orders_filtered,customers_df)
+
+    aggregated_results = DataManupulation.count_orders_state(joined_df)
+
+    aggregated_results.show(50)
+
+    #print(aggregated_results.collect())
+
+    logger.info("this is the end of main")
